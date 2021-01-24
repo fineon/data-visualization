7e0716ece08eaf7508099e7c2ac1fb9e49e31969
Ian <qvnguyen.997@gmail>
2021-01-24 00:33:14 -0800

    testing some model charts with different data, added Highcharts React wrapper library and switched to it, plus some testing

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Highcharts, { chart } from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 import './Dashboard.scss';
+
 
 let allCountries = 'https://api.covid19api.com/summary';
 let canada = 'https://api.covid19api.com/live/country/canada';
 
 
 let currentDate = new Date().toISOString();
-console.log(currentDate);
 
 
 
@@ -17,6 +18,25 @@ export default class Dashboard extends Component {
     state = {
         countries: {},
         canada: [],
+        chartOptions: {
+            chart: {
+                type: 'pie'
+            },
+            title: {
+                text: 'Total Confirmed Cases'
+            },
+            series: [{
+                name: 'cases',
+                colorByPoint: true,
+                data: [{
+                    name: 'Jane',
+                    y: 31
+                }, {
+                    name: 'John',
+                    y: 69
+                }]
+            }]
+        }
     }
 
     //add comparison calculations to stats to emphasize impacts of COVID
@@ -30,76 +50,49 @@ export default class Dashboard extends Component {
             console.log(item);
 
             this.setState({
-                countries: item.data
+                countries: item.data,
+                chartOptions: {
+                    series: [{
+                        // name: item.data.Countries[0].TotalConfirmed,
+                        name: item.data.Global.NewConfirmed,
+                        data: [{
+                            name: 'Jane',
+                            y: 31
+                        }, {
+                            name: 'John',
+                            y: 69
+                        }]
+                    }]
+                }
             });
         });
 
 
+      
 
-        document.addEventListener('DOMContentLoaded', function () {
-            const chart = Highcharts.chart('test', {
-                chart: {
-                    type: 'bar'
-                },
-                title: {
-                    text: 'Total Confirmed Cases'
-                },
-                xAxis: {
-                    categories: ['Apples', 'Bananas', 'Oranges']
-                },
-                yAxis: {
-                    title: {
-                        text: 'Fruit eaten'
-                    }
-                },
-                series: [{
-                    name: 'Jane',
-                    data: [1, 0, 4]
-                }, {
-                    name: 'John',
-                    data: [5, 7, 3]
-                }]
-            });
-        });
 
     }
+
 
     componentDidUpdate() {
 
-        const chart = Highcharts.chart('test', {
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: 'Total Confirmed Cases'
-            },
-            xAxis: {
-                categories: ['Apples', 'Bananas', 'Oranges']
-            },
-            yAxis: {
-                title: {
-                    text: 'Fruit eaten'
-                }
-            },
-            series: [{
-                name: 'Jane',
-                data: [1, 0, 4]
-            }, {
-                name: 'John',
-                data: [5, 7, 3]
-            }]
-        });
-
 
     }
 
-    setStat = () => {
+    setCountry = (event) => {
         //returns undefined, cant read prop of undefined
-        console.log(this.state.countries.Global.TotalConfirmed)
+        // console.log(this.state.countries.Global.TotalConfirmed)
 
-        // this.setState({
-            
-        // })
+        // axios.get(`https://api.covid19api.com/live/country/${event.target.value}`).then(item => {
+        //     console.log(item);
+
+        //     this.setState({
+        //         countries: item.data
+        //     });
+        // });
+
+        console.log(event.target.country.value)
+
     }
 
 
@@ -107,9 +100,7 @@ export default class Dashboard extends Component {
         document.title = "COVID-19 Data Dashboard"
 
         //need to sort data by provinces and create a histogram + all province confirmed cases/canada
-        // let test = this.state.data.filter(item => item.Date === '2021-01-20T00:00:00Z');
-        // console.log(test);
-       
+
 
         return (
             <section>
@@ -120,9 +111,11 @@ export default class Dashboard extends Component {
                     <select name="countries" id="countries">
 
                         {this.state.countries.Countries && this.state.countries.Countries.map(country => {
-                            return <option 
-                            value={country.Country}
-                            onClick={this.setStat}>
+                            return <option
+                                key={country.ID}
+                                name='country'
+                                value={country.Country}
+                                onChange={this.setCountry}>
                                 {country.Country}
                             </option>
                         })}
@@ -130,7 +123,14 @@ export default class Dashboard extends Component {
                     </select>
                 </form>
 
-                <div id="test" > </div>
+                <div className="test">
+                    <HighchartsReact
+                        highcharts={Highcharts}
+                        options={this.state.chartOptions} />
+                </div>
+
+
+                <p>data updated as of {new Date(this.state.countries.Date).toLocaleDateString()}</p>
 
                 <div>
                     <h2>your country here</h2>
@@ -140,11 +140,19 @@ export default class Dashboard extends Component {
                 </div>
 
                 <div>
-                <h2>World Total Cases</h2>
-                {/* <p>{(JSON.stringify(this.state.countries.Global.TotalConfirmed)/7,800,000,000)*100}</p> */}
-                <p>{(780/7800)*100}</p>
-                <p>confirmed cases</p>
-                <p>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview">Yale University</a></p>
+                    <h2>World Total Cases</h2>
+                    {/* <p>{(JSON.stringify(this.state.countries.Global.TotalConfirmed)/7,800,000,000)*100}</p> */}
+                    <p>{(780 / 7800) * 100}</p>
+                    <p>confirmed cases</p>
+                    <p>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview">Yale University</a>
+                    </p>
+                </div>
+
+                <div>
+                    <h2>Province details</h2>
+                    <p>province 1</p>
+                    <p>confirmed cases, recovered, deaths</p>
+                    <p>province 2 etc.</p>
                 </div>
 
 
