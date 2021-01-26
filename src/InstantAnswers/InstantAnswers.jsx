05ab0cdbdd11fc5bd9b86d2976e8a390b2c1e436
Ian <qvnguyen.997@gmail>
2021-01-26 14:38:29 -0800

    added global styles and styling for instant answer section, imported new fonts and color for branding

--- src/InstantAnswers/InstantAnswers.jsx
+++ src/InstantAnswers/InstantAnswers.jsx
@@ -6,33 +6,44 @@ export default function InstantAnswers(props) {
         <div className='answer'>
             <h2 className='answer__title'>Your DuckDuckGo Answer Machine</h2>
 
-            {props.Heading ? <h2 className='answer__title'>{props.Heading}</h2> : null}
+            <div className='answer__title-img-container'>
+                <div className='answer__title-img-container--left'>
+                    {props.Heading ? <h2 className='answer__title--left'>{props.Heading}</h2> : null}
 
-            {props.Image ? <img
-                className='answer__img'
-                src={`https://www.duckduckgo.com${props.Image}`}
-                alt='duckduckgo preview img' /> : null}
+                    {props.Abstract ? <p className='answer__desc'>{props.Abstract}</p> : <a href={props.AbstractURL}
+                        className='answer__link'>{props.AbstractURL}</a>}
+                </div>
 
-            {props.Abstract ? <p className='answer__desc'>{props.Abstract}</p> : <a href={props.AbstractURL}
-                className='answer__link'>{props.AbstractURL}</a>}
+                {props.Image ? <img
+                    className='answer__img'
+                    src={`https://www.duckduckgo.com${props.Image}`}
+                    alt='duckduckgo preview img' /> : null}
 
+
+            </div>
+
+            <div className='answer__infobox-container'>
             {props.Infobox ? props.Infobox.content.map(info => {
                 return (
                     <div
                         className='answer__infobox'
                         key={info.wiki_order}>
-                        <p>{info.label}</p>
-                        <p>{info.value}</p>
+                        <p className='answer__infobox__title'>{info.label}</p>
+                        <p className='answer__infobox__desc'>{info.value}</p>
                     </div>)
             }) : null}
+            </div>
 
-            <form action="" onSubmit={props.searchQuery}>
+            <form 
+            action="" 
+            className='answer__form'
+            onSubmit={props.searchQuery}>
                 <input
-                    className='answer__input'
+                    className='answer__form__input'
                     type="text"
                     name='query'
                     placeholder='Search for topics' />
-                <button className='answer__button'>
+                <button className='answer__form__button'>
                     Search
                 </button>
             </form>
