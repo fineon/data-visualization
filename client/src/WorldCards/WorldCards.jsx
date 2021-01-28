9e675479948b0b3a2bbc8bbe4ded8281499e3c08
Ian <qvnguyen.997@gmail>
2021-01-27 21:58:29 -0800

    added styling for world card component

--- client/src/WorldCards/WorldCards.jsx
+++ client/src/WorldCards/WorldCards.jsx
@@ -4,38 +4,39 @@ import './WorldCards.scss';
 
 export default function WorldCards(props) {
     return (
-        <section>
-            
-            <div>
-                    <h2>World Total Confirmed Cases</h2>
-                    <p>
-                        {props.Global ? (props.Global.TotalConfirmed / 7800000000 * 100).toFixed(2) : null}%
+        <section className='worldcards'>
+            <div className='worldcards__card'>
+                    <h2 className='worldcards__card__title'>Total Confirmed Cases</h2>
+                    <p className='worldcards__card__stat'>
+                        {props.Global ? (props.Global.TotalConfirmed / 7800000000 * 100).toFixed(2) : 'a lot..., jk, check back later for data'}%
                     </p>
-                    <p>confirmed cases</p>
-                    <p>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview">Yale University</a>
+                    <p className='worldcards__card__desc'>of the world population</p>
+                    <p className='worldcards__card__desc'>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview"
+                    className='worldcards__card__link'>Yale University</a>
                     </p>
                 </div>
 
-                <div>
-                    <h2>World Total Death Cases</h2>
-                    <p>
+                <div className='worldcards__card'>
+                    <h2 className='worldcards__card__title'>Total Death Cases</h2>
+                    <p className='worldcards__card__stat'>
                         {props.Global ? (props.Global.TotalDeaths / 7800000000 * 100).toFixed(2) : null}%
                     </p>
-                    <p>death cases</p>
-                    <p>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview">Yale University</a>
+                    <p className='worldcards__card__desc'>of the world population</p>
+                    <p className='worldcards__card__desc'>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview"
+                    className='worldcards__card__link'>Yale University</a>
                     </p>
                 </div>
 
-                <div>
-                    <h2>World Total Recovered Cases</h2>
-                    <p>
+                <div   className='worldcards__card'>
+                    <h2 className='worldcards__card__title'>Total Recovered Cases</h2>
+                    <p className='worldcards__card__stat'>
                         {props.Global ? (props.Global.TotalRecovered / 7800000000 * 100).toFixed(2) : null}%
                     </p>
-                    <p>recovered cases</p>
-                    <p>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview">Yale University</a>
+                    <p className='worldcards__card__desc'>of the world population</p>
+                    <p className='worldcards__card__desc'>source: <a href="https://yaleglobal.yale.edu/content/world-population-2020-overview"
+                    className='worldcards__card__link'>Yale University</a>
                     </p>
                 </div>
-
         </section>
     )
 }
